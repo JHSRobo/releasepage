689dafbbd8c5815a432008fb2982405adb5d3e33
Alden Parker <32986873+aldenparker@users.noreply.github.com>
2019-10-15 20:58:55 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+cd /home/jhsrobo/Github
+
+git clone https://github.com/JHSRobo/ROVMIND.git
+
+cd /home/jhsrobo/Github/ROVMIND/ros_workspace/src/
+git clone https://github.com/JHSRobo/copilot-page.git
+
+cd /home/jhsrobo/Github/ROVMIND/ros_workspace
+catkin_make
