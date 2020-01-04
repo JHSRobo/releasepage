a62a7204a6957fb66774c243e3a668d1bbaf562e
Michael Equi <equim20@student.jhs.net>
2020-01-03 22:26:48 -0800

    added clone rov description and rov localization

--- tcu_build.sh
+++ tcu_build.sh
@@ -22,7 +22,6 @@ git clone --branch V0.1 https://github.com/JHSRobo/hardware_interface.git
 git clone --branch V0.1 https://github.com/JHSRobo/ms5837.git
 git clone --branch V0.0 https://github.com/JHSRobo/rov_control.git
 git clone --branch V0.1 https://github.com/JHSRobo/rov_control_interface.git
-# git clone --branch V0.1 https://github.com/JHSRobo/rov_description.git
 # git clone --branch V0.1 https://github.com/JHSRobo/sensor_readout.git
 git clone --branch V0.1 https://github.com/JHSRobo/sht31.git
 git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
