28b6bed83b5818279f899e0ae31c02e4cd1ddbb2
ajparker <ajparker1401@gmail.com>
2021-04-17 13:45:58 -0700

    update

--- tcu_build.sh
+++ tcu_build.sh
@@ -5,7 +5,7 @@ cd ~/Github
 rm -rf ./ROVMIND
 rm -rf ./RPICamera
 
-git clone --branch V2.5.1 https://github.com/JHSRobo/ROVMIND.git
+git clone --branch V2.5.2 https://github.com/JHSRobo/ROVMIND.git
 git clone --branch V0.5 https://github.com/JHSRobo/RPICamera.git
 
 mv ~/Github/RPICamera/camera_viewer/ ~/Github/ROVMIND/ros_workspace/src/
@@ -15,7 +15,7 @@ git clone --branch V0.1 https://github.com/JHSRobo/bmp280.git
 git clone --branch V0.2 https://github.com/JHSRobo/bno055.git
 git clone --branch V0.0 https://github.com/JHSRobo/copilot_interface.git
 git clone --branch V0.1 https://github.com/JHSRobo/hardware_interface.git
-git clone https://github.com/JHSRobo/keyboard-teleop.git
+git clone https://github.com/JHSRobo/keyboard_teleop.git
 git clone --branch V0.3 https://github.com/JHSRobo/ms5837.git
 git clone --branch V0.1 https://github.com/JHSRobo/rov_control_interface.git
 git clone --branch V0.1 https://github.com/JHSRobo/sht31.git
