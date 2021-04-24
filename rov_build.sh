1df2c9c8ce8ad103a6f6043b26b6795d7b91459e
Alden Parker <32986873+aldenparker@users.noreply.github.com>
2021-04-24 12:55:03 -0700

    Update rov_build.sh

--- rov_build.sh
+++ rov_build.sh
@@ -13,14 +13,14 @@ mv ~/Github/RPICamera/camera_viewer/ ~/Github/ROVMIND/ros_workspace/src/
 cd ~/Github/ROVMIND/ros_workspace/src/
 git clone --branch V0.1 https://github.com/JHSRobo/bmp280.git
 git clone --branch V0.2 https://github.com/JHSRobo/bno055.git
-# git clone --branch V0.0 https://github.com/JHSRobo/copilot_interface.git
+git clone --branch V0.0 https://github.com/JHSRobo/copilot_interface.git
 git clone --branch V1.0 https://github.com/JHSRobo/hardware_interface.git
-# git clone --branch v1.0 https://github.com/JHSRobo/keyboard-teleop.git
+git clone --branch v1.0 https://github.com/JHSRobo/keyboard-teleop.git
 git clone --branch V0.3 https://github.com/JHSRobo/ms5837.git
-# git clone --branch V0.1 https://github.com/JHSRobo/rov_control_interface.git
+git clone --branch V0.1 https://github.com/JHSRobo/rov_control_interface.git
 git clone --branch V0.1 https://github.com/JHSRobo/sht31.git
-# git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
-# git clone --branch V0.0 https://github.com/JHSRobo/tcu_touchscreen.git
+git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
+git clone --branch V0.0 https://github.com/JHSRobo/tcu_touchscreen.git
 git clone --branch V0.0 https://github.com/JHSRobo/vector_drive.git
 git clone --branch V1.4 https://github.com/JHSRobo/gpio_control.git
 
