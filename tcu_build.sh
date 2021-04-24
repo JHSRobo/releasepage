23a778e26e4fffcd7dcfdf7897c404a1af510f8c
Alden Parker <32986873+aldenparker@users.noreply.github.com>
2021-04-24 11:20:27 -0700

    Update tcu_build.sh

--- tcu_build.sh
+++ tcu_build.sh
@@ -14,7 +14,7 @@ cd ~/Github/ROVMIND/ros_workspace/src/
 git clone --branch V0.1 https://github.com/JHSRobo/bmp280.git
 git clone --branch V0.2 https://github.com/JHSRobo/bno055.git
 git clone --branch V0.0 https://github.com/JHSRobo/copilot_interface.git
-git clone --branch V1.0 https://github.com/JHSRobo/hardware_interface.git
+# git clone --branch V1.0 https://github.com/JHSRobo/hardware_interface.git
 git clone --branch V1.0 https://github.com/JHSRobo/keyboard_teleop.git
 git clone --branch V0.3 https://github.com/JHSRobo/ms5837.git
 git clone --branch V0.1 https://github.com/JHSRobo/rov_control_interface.git
@@ -22,7 +22,7 @@ git clone --branch V0.1 https://github.com/JHSRobo/sht31.git
 git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
 git clone --branch V0.0 https://github.com/JHSRobo/tcu_touchscreen.git
 git clone --branch V0.0 https://github.com/JHSRobo/vector_drive.git
-git clone --branch V1.1 https://github.com/JHSRobo/electromag.git
+git clone --branch V1.1 https://github.com/JHSRobo/gpio_control.git
 
 # git clone --branch 2.2.9 https://github.com/IntelRealSense/realsense-ros.git
 # git clone https://github.com/JHSRobo/rov_localization.git
