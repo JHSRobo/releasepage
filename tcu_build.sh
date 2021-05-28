f7aa839dac85f25a4ebc675152e4d731a39dec5b
Andrew Grindstaff <ajgrinds03@gmail.com>
2021-05-28 13:55:00 -0700

    Update tcu_build.sh

--- tcu_build.sh
+++ tcu_build.sh
@@ -6,7 +6,7 @@ rm -rf ./ROVMIND
 rm -rf ./RPICamera
 
 git clone --branch V2.5.3 https://github.com/JHSRobo/ROVMIND.git
-git clone --branch V1.3 https://github.com/JHSRobo/RPICamera.git
+git clone --branch V2.0 https://github.com/JHSRobo/RPICamera.git
 
 mv ~/Github/RPICamera/camera_viewer/ ~/Github/ROVMIND/ros_workspace/src/
 rm -rf ~/Github/RPICamera
