911f2bbd91fee6b318a95c69f3e21b17b4dc89af
Andrew Grindstaff <ajgrinds03@gmail.com>
2021-05-31 11:26:58 -0700

    Update rov_build.sh

--- rov_build.sh
+++ rov_build.sh
@@ -5,7 +5,7 @@ cd ~/Github
 rm -rf ./ROVMIND
 rm -rf ./RPICamera
 
-git clone --branch V2.5.3 https://github.com/JHSRobo/ROVMIND.git
+git clone --branch V2.6 https://github.com/JHSRobo/ROVMIND.git
 git clone --branch V2.0 https://github.com/JHSRobo/RPICamera.git
 
 mv ~/Github/RPICamera/camera_viewer/ ~/Github/ROVMIND/ros_workspace/src/
