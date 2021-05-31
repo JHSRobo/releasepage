060f94d88e501552689dd3d468771a9a52ee88ca
Andrew Grindstaff <ajgrinds03@gmail.com>
2021-05-31 11:51:15 -0700

    Update rov_build.sh

--- rov_build.sh
+++ rov_build.sh
@@ -23,7 +23,7 @@ git clone --branch V0.1 https://github.com/JHSRobo/sht31.git
   git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
 git clone --branch V0.0 https://github.com/JHSRobo/vector_drive.git
 git clone --branch V1.4 https://github.com/JHSRobo/gpio_control.git
-git clone --branch V1.0 https://github.com/JHSRobo/micro_rov.git
+git clone --branch V1.0.1 https://github.com/JHSRobo/micro_rov.git
 
 # git clone --branch 2.2.9 https://github.com/IntelRealSense/realsense-ros.git
 # git clone https://github.com/JHSRobo/rov_localization.git
