c08e45a75d062094dc92274055527ead437559cc
Andrew Grindstaff <ajgrinds03@gmail.com>
2021-06-05 10:30:30 -0700

    Update tcu_build.sh

--- tcu_build.sh
+++ tcu_build.sh
@@ -12,27 +12,11 @@ mv ~/Github/RPICamera/camera_viewer/ ~/Github/ROVMIND/ros_workspace/src/
 rm -rf ~/Github/RPICamera
 
 cd ~/Github/ROVMIND/ros_workspace/src/
-git clone --branch V0.1 https://github.com/JHSRobo/bmp280.git
-git clone --branch V0.2 https://github.com/JHSRobo/bno055.git
 git clone --branch V0.2 https://github.com/JHSRobo/copilot_interface.git
 git clone --branch V1.1 https://github.com/JHSRobo/hardware_interface.git
 git clone --branch V2.5 https://github.com/JHSRobo/keyboard_teleop.git
 git clone --branch V0.2 https://github.com/JHSRobo/rov_control_interface.git
-git clone --branch V0.1 https://github.com/JHSRobo/sht31.git
 git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
-git clone --branch V0.0 https://github.com/JHSRobo/tcu_touchscreen.git
-git clone --branch V0.0 https://github.com/JHSRobo/vector_drive.git
-
-# git clone --branch 2.2.9 https://github.com/IntelRealSense/realsense-ros.git
-# git clone https://github.com/JHSRobo/rov_localization.git
-# git clone https://github.com/JHSRobo/rov_description.git
-# git clone --branch V0.1 https://github.com/JHSRobo/sensor_readout.git
-# git clone --branch V0.0 https://github.com/JHSRobo/raspicam_node.git
-# git clone --branch V0.0 https://github.com/JHSRobo/realsense2_description.git
-# git clone --branch V0.0 https://github.com/JHSRobo/rov_control.git
-# git clone --branch V0.0 https://github.com/JHSRobo/doc.git
-# git clone --branch V1.0 https://github.com/JHSRobo/copilot_page.git
-# git clone --branch V0.1 https://github.com/JHSRobo/depth_hold.git
 
 cd ~/Github/ROVMIND/ros_workspace
 
