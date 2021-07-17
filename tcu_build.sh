5df023a5791e3bfcecae57ea4858644875445459
Andrew Grindstaff <ajgrinds03@gmail.com>
2021-07-17 09:58:42 -0700

    Update tcu_build.sh

--- tcu_build.sh
+++ tcu_build.sh
@@ -16,7 +16,7 @@ rm -rf ~/Github/RPICamera
 cd ~/Github/ROVMIND/ros_workspace/src/
 git clone --branch V0.4.2 https://github.com/JHSRobo/copilot_interface.git
 git clone --branch V1.1 https://github.com/JHSRobo/hardware_interface.git
-git clone --branch V3.0 https://github.com/JHSRobo/keyboard_teleop.git
+git clone --branch V3.1 https://github.com/JHSRobo/keyboard_teleop.git
 git clone --branch V3.0 https://github.com/JHSRobo/rov_control_interface.git
 git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
 git clone --branch V0.0 https://github.com/JHSRobo/vector_drive.git
