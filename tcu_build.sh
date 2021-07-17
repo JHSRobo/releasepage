eab30d2cd08907eb9689c16c931bd729aee1889f
Andrew Grindstaff <ajgrinds03@gmail.com>
2021-07-17 12:10:29 -0700

    Update tcu_build.sh

--- tcu_build.sh
+++ tcu_build.sh
@@ -7,7 +7,7 @@ cat ~/.bashrc | grep "cameras" || (echo "alias cameras=\"rosrun camera_viewer sw
 rm -rf ./ROVMIND
 rm -rf ./RPICamera
 
-git clone --branch V2.10.1 https://github.com/JHSRobo/ROVMIND.git
+git clone --branch V2.10.2 https://github.com/JHSRobo/ROVMIND.git
 git clone --branch V3.1 https://github.com/JHSRobo/RPICamera.git
 
 mv ~/Github/RPICamera/camera_viewer/ ~/Github/ROVMIND/ros_workspace/src/
@@ -16,7 +16,7 @@ rm -rf ~/Github/RPICamera
 cd ~/Github/ROVMIND/ros_workspace/src/
 git clone --branch V0.4.2 https://github.com/JHSRobo/copilot_interface.git
 git clone --branch V1.1 https://github.com/JHSRobo/hardware_interface.git
-git clone --branch V3.1.1 https://github.com/JHSRobo/keyboard_teleop.git
+git clone --branch V3.2 https://github.com/JHSRobo/keyboard_teleop.git
 git clone --branch V0.4.2 https://github.com/JHSRobo/rov_control_interface.git
 git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
 git clone --branch V0.0 https://github.com/JHSRobo/vector_drive.git
