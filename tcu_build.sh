d502121c75d03267702dd8de6fddf269d8b33954
Orcabadger <83510769+Orcabadger@users.noreply.github.com>
2022-03-19 13:49:33 -0700

    Update tcu_build.sh

--- tcu_build.sh
+++ tcu_build.sh
@@ -14,7 +14,7 @@ mv ~/Github/RPICamera/camera_viewer/ ~/Github/ROVMIND/ros_workspace/src/
 rm -rf ~/Github/RPICamera
 
 cd ~/Github/ROVMIND/ros_workspace/src/
-git clone --branch V0.4.2 https://github.com/JHSRobo/copilot_interface.git
+git clone --branch V0.5 https://github.com/JHSRobo/copilot_interface.git
 git clone --branch V1.1 https://github.com/JHSRobo/hardware_interface.git
 git clone --branch V3.4 https://github.com/JHSRobo/keyboard_teleop.git
 git clone --branch V0.4.2 https://github.com/JHSRobo/rov_control_interface.git
