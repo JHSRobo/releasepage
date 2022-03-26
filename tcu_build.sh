964182641aea5e56ce00c0b4f8eb114bd3d19ad6
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2022-03-26 12:05:59 -0700

    Update tcu_build.sh

--- tcu_build.sh
+++ tcu_build.sh
@@ -21,7 +21,7 @@ git clone --branch V3.4 https://github.com/JHSRobo/keyboard_teleop.git
 git clone --branch V1.0 https://github.com/JHSRobo/rov_control_interface.git
 git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
 git clone --branch V0.0 https://github.com/JHSRobo/vector_drive.git
-git clone --branch V1.2 https://github.com/JHSRobo/depth_hold.git
+git clone --branch V1.3 https://github.com/JHSRobo/depth_hold.git
 
 
 cd ~/Github/ROVMIND/ros_workspace
