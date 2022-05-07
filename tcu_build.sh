7c90e151c7b12e64e83cd5ccfa8269196cf90996
Alex Bertran <70992408+aBertran1@users.noreply.github.com>
2022-05-07 09:49:22 -0700

    Update tcu_build.sh

--- tcu_build.sh
+++ tcu_build.sh
@@ -7,7 +7,7 @@ cat ~/.bashrc | grep "cameras" || (echo "alias cameras=\"rosrun camera_viewer sw
 rm -rf ./ROVMIND
 rm -rf ./RPICamera
 
-git clone --branch V2.12 https://github.com/JHSRobo/ROVMIND.git
+git clone --branch V2.13 https://github.com/JHSRobo/ROVMIND.git
 git clone --branch V3.9 https://github.com/JHSRobo/RPICamera.git
 
 mv ~/Github/RPICamera/camera_viewer/ ~/Github/ROVMIND/ros_workspace/src/
