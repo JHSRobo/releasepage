da5257e12bda40f634bc3f94d2925bcc14fa9c36
Alex Bertran <70992408+aBertran1@users.noreply.github.com>
2022-05-07 10:11:17 -0700

    Update tcu_build.sh

--- tcu_build.sh
+++ tcu_build.sh
@@ -8,7 +8,7 @@ rm -rf ./ROVMIND
 rm -rf ./RPICamera
 
 git clone --branch V2.13 https://github.com/JHSRobo/ROVMIND.git
-git clone --branch V3.9 https://github.com/JHSRobo/RPICamera.git
+git clone --branch V3.10 https://github.com/JHSRobo/RPICamera.git
 
 mv ~/Github/RPICamera/camera_viewer/ ~/Github/ROVMIND/ros_workspace/src/
 rm -rf ~/Github/RPICamera
@@ -17,7 +17,7 @@ cd ~/Github/ROVMIND/ros_workspace/src/
 git clone --branch V1.5 https://github.com/JHSRobo/gpio_control.git # necessary for the camera viewer
 git clone --branch V0.5 https://github.com/JHSRobo/copilot_interface.git
 git clone --branch V1.1 https://github.com/JHSRobo/hardware_interface.git
-git clone --branch V3.5 https://github.com/JHSRobo/keyboard_teleop.git
+git clone --branch V3.6 https://github.com/JHSRobo/keyboard_teleop.git
 git clone --branch V1.2 https://github.com/JHSRobo/rov_control_interface.git
 git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
 git clone --branch V0.0 https://github.com/JHSRobo/vector_drive.git
