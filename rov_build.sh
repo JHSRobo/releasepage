884c8ff48fae6274156336178230d091cd41ed1c
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2022-10-08 10:24:44 -0700

    Removed microROV and some sensor repositories

--- rov_build.sh
+++ rov_build.sh
@@ -12,17 +12,13 @@ mv ~/Github/RPICamera/streamer/ ~/Github/
 rm -rf ~/Github/RPICamera
 
 cd ~/Github/ROVMIND/ros_workspace/src/
-#git clone --branch V0.1 https://github.com/JHSRobo/bmp280.git
-#git clone --branch V0.2 https://github.com/JHSRobo/bno055.git
 git clone --branch V1.20.4 https://github.com/JHSRobo/copilot_interface.git
 git clone --branch V1.1 https://github.com/JHSRobo/hardware_interface.git
 git clone --branch V0.6 https://github.com/JHSRobo/depth_sensor.git
 git clone --branch V1.5.1 https://github.com/JHSRobo/rov_control_interface.git
-#git clone --branch V0.1 https://github.com/JHSRobo/sht31.git
 git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
 git clone --branch V0.0 https://github.com/JHSRobo/vector_drive.git
 git clone --branch V1.5 https://github.com/JHSRobo/gpio_control.git
-git clone --branch V1.1.3 https://github.com/JHSRobo/micro_rov.git
 git clone --branch V1.2 https://github.com/JHSRobo/drq_ros.git
 git clone --branch V1.0 https://github.com/JHSRobo/pi_temp.git
 
