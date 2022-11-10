326e1cd068c6be7de369bf4084c1a5e4891fbc83
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2022-11-09 22:54:51 -0800

    Update tcu_build.sh

--- tcu_build.sh
+++ tcu_build.sh
@@ -8,27 +8,39 @@ fi
 
 cd ~/Github
 
-cat ~/.bashrc | grep "cameras" || (echo "alias cameras=\"rosrun camera_viewer switcher.py\"" >> ~/.bashrc) # Add camera command
-cat ~/.bashrc | grep "topside" || (echo "alias topside=\"roslaunch launch_files topside.launch"" >> ~/.bashrc) # Add camera command
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-rm -rf ./ROVMIND
-rm -rf ./RPICamera
+dpkg -l curl || apt install curl -y # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+
+apt update -y
+apt install ros-noetic-ros-base -y
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+echo "source ~/GIthub/ROVMIND/ros_workspace/devel/setup.bash" >> ~/.bashrc
+echo "export ROS_MASTER_URI=http://master:11311" >> ~/.bashrc
+echo "alias cameras=\"rosrun camera_viewer switcher.py\"" >> ~/.bashrc
+echo "export ROS_HOSTNAME=master" >> ~/.bashrc
+echo "alias topside=\"cd ~/Github/ROVMIND/ros_workspace && roslaunch launch_files topside.launch\"" >> ~/.bashrc # Add camera command
+source ~/.bashrc
+
+apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+
+rosdep init
+rosdep update
 
 git clone --branch V2.13 https://github.com/JHSRobo/ROVMIND.git
-git clone --branch V3.12.1 https://github.com/JHSRobo/RPICamera.git
-
-mv ~/Github/RPICamera/camera_viewer/ ~/Github/ROVMIND/ros_workspace/src/
-rm -rf ~/Github/RPICamera
 
 cd ~/Github/ROVMIND/ros_workspace/src/
-git clone --branch V1.5 https://github.com/JHSRobo/gpio_control.git # necessary for the camera viewer
-git clone --branch V1.20.5 https://github.com/JHSRobo/copilot_interface.git
-git clone --branch V1.1 https://github.com/JHSRobo/hardware_interface.git
-git clone --branch V3.7 https://github.com/JHSRobo/keyboard_teleop.git
-git clone --branch V1.5.1 https://github.com/JHSRobo/rov_control_interface.git
-git clone --branch V0.0 https://github.com/JHSRobo/vector_drive.git
-git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
-#git clone --branch V1.3 https://github.com/JHSRobo/depth_hold.git
+git clone https://github.com/JHSRobo/RPICamera.git
+git clone https://github.com/JHSRobo/gpio_control.git # necessary for the camera viewer
+git clone https://github.com/JHSRobo/copilot_interface.git
+git clone https://github.com/JHSRobo/hardware_interface.git
+git clone https://github.com/JHSRobo/keyboard_teleop.git
+git clone https://github.com/JHSRobo/rov_control_interface.git
+git clone https://github.com/JHSRobo/vector_drive.git
+git clone https://github.com/JHSRobo/tcu_board_msgs.git
+git clone https://github.com/JHSRobo/RPICamera.git
 
 cd ~/Github/ROVMIND/ros_workspace
 
