379416bdf2a7ca4248018efe5e0526f19384dc9c
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2022-11-13 18:52:03 -0800

    Update tcu_build.sh

--- tcu_build.sh
+++ tcu_build.sh
@@ -9,26 +9,18 @@ fi
 cd ~/Github
 
 cat ~/.bashrc | grep "cameras" || (echo "alias cameras=\"rosrun camera_viewer switcher.py\"" >> ~/.bashrc) # Add camera command
-#cat ~/.bashrc | grep "topside" || (echo "alias topside=\"roslaunch launch_files topside.launch"" >> ~/.bashrc) # Add camera command
 
 rm -rf ./ROVMIND
-rm -rf ./RPICamera
 
 git clone --branch V2.13 https://github.com/JHSRobo/ROVMIND.git
-git clone --branch V3.12.1 https://github.com/JHSRobo/RPICamera.git
-
-mv ~/Github/RPICamera/camera_viewer/ ~/Github/ROVMIND/ros_workspace/src/
-rm -rf ~/Github/RPICamera
 
 cd ~/Github/ROVMIND/ros_workspace/src/
-git clone --branch V1.5 https://github.com/JHSRobo/gpio_control.git # necessary for the camera viewer
-git clone --branch V1.20.5 https://github.com/JHSRobo/copilot_interface.git
-git clone --branch V1.1 https://github.com/JHSRobo/hardware_interface.git
-git clone --branch V3.7 https://github.com/JHSRobo/keyboard_teleop.git
-git clone --branch V1.5.1 https://github.com/JHSRobo/rov_control_interface.git
-git clone --branch V0.0 https://github.com/JHSRobo/vector_drive.git
-git clone --branch V0.0 https://github.com/JHSRobo/tcu_board_msgs.git
-#git clone --branch V1.3 https://github.com/JHSRobo/depth_hold.git
+git clone https://github.com/JHSRobo/RPICamera.git
+git clone https://github.com/JHSRobo/gpio_control.git # necessary for the camera viewer
+git clone https://github.com/JHSRobo/copilot_interface.git
+git clone https://github.com/JHSRobo/keyboard_teleop.git
+git clone https://github.com/JHSRobo/rov_control_interface.git
+git clone https://github.com/JHSRobo/tcu_board_msgs.git
 
 cd ~/Github/ROVMIND/ros_workspace
 
